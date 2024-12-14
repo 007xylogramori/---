feee81b1b720b05727766b7fa4601cbb992979db
Himanshu <xylogramori@gmail.com>
2024-12-15 02:08:17 +0530

    Middeleware added

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -7,18 +7,18 @@ import {
   changeCurrentPassword,
   getCurrentUser,
 } from "../controllers/user.controller.js";
-
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
-
-router.route("/logout").get( logoutUser);
+//secured routes
+router.route("/logout").get(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password").post( changeCurrentPassword);
-router.route("/current-user").get( getCurrentUser);
-router.route("/update-account").patch( updateAccountDetails)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 
 export default router;
